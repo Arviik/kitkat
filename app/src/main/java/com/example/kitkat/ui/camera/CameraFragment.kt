aec70f3b35bec6f3868a3ce528024b0a04bf6521
Kyllian Maignan <shokkzu@gmail.com>
2024-11-17 15:16:36 +0100

    feat(camera): Camera capture done, bugs on Jira

--- app/src/main/java/com/example/kitkat/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/kitkat/ui/camera/CameraFragment.kt
@@ -1,21 +1,29 @@
 package com.example.kitkat.ui.camera
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.graphics.Color
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Video
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.video.FallbackStrategy
+import androidx.camera.video.MediaStoreOutputOptions
 import androidx.camera.video.Quality
 import androidx.camera.video.QualitySelector
 import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
 import androidx.camera.video.VideoCapture
+import androidx.camera.video.VideoRecordEvent
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -28,6 +36,8 @@ import com.example.kitkat.MainActivity.Companion.REQUIRED_PERMISSIONS
 import com.example.kitkat.R
 import com.example.kitkat.databinding.FragmentCameraBinding
 import com.google.common.util.concurrent.ListenableFuture
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class CameraFragment : Fragment() {
 
@@ -38,6 +48,10 @@ class CameraFragment : Fragment() {
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider> //Permet de mieux gérer la caméra apperement, Future = asynchrone
     private var isUsingBackCamera = false
     private var isRecording = false
+    private var isVideoCaptureInitialized = false
+
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
 
     val qualitySelector = QualitySelector.fromOrderedList(
         listOf(Quality.UHD, Quality.FHD, Quality.HD, Quality.SD),
@@ -56,8 +70,11 @@ class CameraFragment : Fragment() {
 
         preview.setSurfaceProvider(binding.previewView.surfaceProvider)
 
-        val recorder = Recorder.Builder().setQualitySelector(qualitySelector).build()
-        val videoCapture = VideoCapture.withOutput(recorder)
+        if (!isVideoCaptureInitialized){
+            val recorder = Recorder.Builder().setQualitySelector(qualitySelector).build()
+            videoCapture = VideoCapture.withOutput(recorder)
+            isVideoCaptureInitialized = true
+        }
 
         cameraProvider.unbindAll()
         cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, preview, videoCapture)
@@ -76,15 +93,70 @@ class CameraFragment : Fragment() {
         }, ContextCompat.getMainExecutor(activity as MainActivity))
     }
 
+    @SuppressLint("MissingPermission")
     private fun toggleRecording() {
-        isRecording = !isRecording
-        if (isRecording) {
-            binding.btnRecord.setBackgroundColor(Color.RED)
-            binding.recordIndicator.visibility = ImageView.VISIBLE
-        } else {
+        val videoCapture = videoCapture ?: return
 
+        if (isRecording) {
+            recording?.pause()
             binding.btnRecord.setBackgroundColor(Color.BLACK)
             binding.recordIndicator.visibility = ImageView.INVISIBLE
+
+            isRecording = false
+        } else {
+            if (recording != null){
+                recording?.resume()
+                binding.btnRecord.setBackgroundColor(Color.RED)
+                binding.recordIndicator.visibility = ImageView.VISIBLE
+                isRecording = true
+                return
+            }
+
+            //Début enregistrement
+            val name = "KitKatRecord-" + SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.FRANCE).format(System.currentTimeMillis()) + ".mp4"
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+                put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+            }
+            val mediaStoreOutput = MediaStoreOutputOptions.Builder(requireContext().contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+                                    .setContentValues(contentValues)
+                                    .build()
+
+            recording = videoCapture.output
+                .prepareRecording(requireContext(), mediaStoreOutput)
+                .apply { withAudioEnabled() }
+                .start(ContextCompat.getMainExecutor(requireContext())) { recordEvent ->
+                    when (recordEvent) {
+                        is VideoRecordEvent.Start -> {
+                            Toast.makeText(context, "Enregistrement commencé", Toast.LENGTH_SHORT).show()
+                        }
+                        is VideoRecordEvent.Resume -> {
+                            Toast.makeText(context, "Enregistrement repris", Toast.LENGTH_SHORT).show()
+                        }
+                        is VideoRecordEvent.Pause -> {
+                            Toast.makeText(context, "Enregistrement mis en pause", Toast.LENGTH_SHORT).show()
+                        }
+                        is VideoRecordEvent.Finalize -> {
+                            if (!recordEvent.hasError()) {
+                                Toast.makeText(context, "Enregistrement exporté avec succès dans votre galerie", Toast.LENGTH_LONG).show()
+                            } else {
+                                Toast.makeText(context, "Erreur : ${recordEvent.error}", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                }
+
+            binding.btnRecord.setBackgroundColor(Color.RED)
+            binding.recordIndicator.visibility = ImageView.VISIBLE
+            isRecording = true
+        }
+    }
+
+    private fun exportVideo() {
+        if (recording != null){
+            recording?.stop()
+            recording = null
+            isRecording = false
         }
     }
 
@@ -142,6 +214,10 @@ class CameraFragment : Fragment() {
                 }
             }
             true
+        }
+
+        binding.btnFinishVideo.setOnClickListener {
+            exportVideo()
         }
 
         return root
